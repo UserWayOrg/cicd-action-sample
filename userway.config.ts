f97eac33651b4ccd2b816a8502bb20cf605efaa3
Oleksandr Zalizniak <alex@userway.org>
2024-06-09 11:43:07 -0400

    Project name fixed

--- userway.config.ts
+++ userway.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "@userway/cicd-core";
 
 export default {
   organization: "alex-userway-org",
-  project: "cicd-bitbucket-sample",
+  project: "cicd-action-sample",
   token: process.env.USERWAY_TOKEN,
   reportPaths: ["./uw-a11y-reports"]
 } satisfies Config;
