f0d4e81295a8dc72d63334cb70c1cb330a043171
Oleksandr Zalizniak <alex@userway.org>
2024-07-31 17:26:50 -0400

    Moved to prod CICD

--- userway.config.ts
+++ userway.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from "@userway/cicd-core";
 
 export default {
-  organization: "alex-userway-org",
+  organization: "support-userway-org",
   project: "cicd-action-sample",
   token: process.env.USERWAY_TOKEN,
-  reportPaths: ["./uw-a11y-reports"]
+  reportPaths: ["./uw-a11y-reports"],
+  server: 'https://api.userway.org',
 } satisfies Config;
